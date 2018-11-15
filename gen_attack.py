cde7067a96820fff5f5a82da67e0bb7b4af3a472
Evan Lavender <evanlavender@pop-os.localdomain>
2018-11-14 20:28:39 -0500

    minor documentation

--- gen_attack.py
+++ gen_attack.py
@@ -18,16 +18,18 @@ class GenAttack:
 
         # create a maximizing fitness value
         creator.create("FitnessMax", base.Fitness, weights=(1.0,))
-        # create an individual that is a list of values
+        # create an individual that is a list of values to be added to image
         creator.create("Individual", list, fitness=creator.FitnessMax)
 
+        # random distribution - bernoulli(p) * U(-delta, delta)
         def distribution():
             return np.random.binomial(n=1, p=self.mut_prob) * np.random.uniform(low=-self.dist_delta,
                                                                                 high=self.dist_delta)
 
-        # create uniform sample function
+        # individual of random values
         self.toolbox.register("individual", tools.initRepeat, creator.Individual,
                               distribution, n=2 ** 2)
+        # population of random individuals
         self.toolbox.register("population", tools.initRepeat, list, self.toolbox.individual)
 
     def attack(self, orig_img, target, num_gen):
