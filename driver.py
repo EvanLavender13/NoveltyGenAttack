56ceb890d2c4dc5973400f3a14a2dd290c949938
Evan Lavender <evanlavender@pop-os.localdomain>
2018-11-16 18:31:02 -0500

    fleshing out the algorithm

--- driver.py
+++ driver.py
@@ -1,5 +1,5 @@
 from gen_attack import GenAttack
 
-gen = GenAttack(dist_delta=1, step_size=1, pop_size=1, cx_prob=1, mut_prob=1)
+gen = GenAttack(dist_delta=1, step_size=1, pop_size=1, cx_prob=1, mut_prob=1, model=None)
 
-gen.attack(orig_img=None, target=None, model=None, num_gen=1)
+gen.attack(orig_img=None, target=None, pop_size=25, num_gen=1)
--- gen_attack.py
+++ gen_attack.py
@@ -5,12 +5,13 @@ from deap import tools
 
 
 class GenAttack:
-    def __init__(self, dist_delta, step_size, pop_size, cx_prob, mut_prob):
+    def __init__(self, dist_delta, step_size, pop_size, cx_prob, mut_prob, model):
         self.dist_delta = dist_delta
         self.step_size = step_size
         self.pop_size = pop_size
         self.cx_prob = cx_prob
         self.mut_prob = mut_prob
+        self.model = model
 
         self.toolbox = base.Toolbox()
 
@@ -26,27 +27,52 @@ class GenAttack:
             return np.random.binomial(n=1, p=self.mut_prob) * np.random.uniform(low=-self.dist_delta,
                                                                                 high=self.dist_delta)
 
+        def evaluate(individual):
+            # TODO: do stuff to get model prediction
+            target_prediction = sum(individual)  # just for demonstration
+            other_prediction = 1  # max prediction of other label != target
+
+            return (np.log10(target_prediction) - np.log10(other_prediction),)
+
         # individual of random values
         self.toolbox.register("individual", tools.initRepeat, creator.Individual,
                               distribution, n=2 ** 2)
         # population of random individuals
         self.toolbox.register("population", tools.initRepeat, list, self.toolbox.individual)
+        # evaluation function
+        self.toolbox.register("evaluate", evaluate)
 
-    def attack(self, orig_img, target, model, num_gen):
+    def attack(self, orig_img, target, pop_size, num_gen):
         # initialize population
-        pop = self.toolbox.population(n=25)
+        pop = self.toolbox.population(n=pop_size)
 
         for ind in pop:
             print(ind)
 
         for g in range(num_gen):
-            continue
-            # compute fitnesss
+            # evaluate the entire population
+            fitnesses = map(self.toolbox.evaluate, pop)
+            for ind, fit in zip(pop, fitnesses):
+                print("fit=", fit)
+                ind.fitness.values = fit
 
             # check if best individual == target (keep best individual)
 
-            # roulette selection
+            # select the next generation individuals
+            offspring = list(map(self.toolbox.clone, tools.selRoulette(pop, len(pop))))
 
-            # uniform crossover
+            # mate the offspring
+            for child1, child2 in zip(offspring[::2], offspring[1::2]):
+                if np.random.random() < self.cx_prob:
+                    # TODO: uniform crossover (f(child1) / (f(child1) + f(child2))
+                    del child1.fitness.values
+                    del child2.fitness.values
 
-            # mutation
+            # mutate the offspring
+            for mutant in offspring:
+                if np.random.random() < self.mut_prob:
+                    # TODO: mutate (bernoulli(p) * uniform(-delta, delta)
+                    del mutant.fitness.values
+
+            # replace population with new offspring
+            pop[:] = offspring
